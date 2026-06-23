4b3a224a3282ac3362d0b11bfbf6f33c2244c972
ilai-24 <Ilai.Amrami@s.afeka.ac.il>
2026-06-23 12:19:26 +0300

    add articles logic

---
+++ src/Ilai_Amrami_Amir_Kashani/InvalidTitleException.java
@@ -0,0 +1,13 @@
+package Ilai_Amrami_Amir_Kashani;
+
+public class InvalidTitleException extends RuntimeException {
+    public InvalidTitleException()
+    {
+        super();
+    }
+    public InvalidTitleException(String message)
+    {
+        super(message);
+    }
+
+}
